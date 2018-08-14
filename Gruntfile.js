64fb2618de67de2c0030342bd3ea07155e146c8e
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-08-15 10:42:24 +1200

    added all grunt tasks into the gruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,21 +11,65 @@ module.exports = function(grunt){
             }]
           }
       },
+      jshint: {
+          files: ["*.js", "js/script.js"],
+          options: {
+              globals:{
+                  jQuery: true
+              }
+          }
+      },
+      csslint: {
+          strict: {
+            options: {
+              import: 2
+            },
+            src: ["css/style.css"]
+          }
+      },
+      uglify:{
+          my_target:{
+              files: {
+                  "js/script.min.js":["js/script.js"]
+              }
+          }
+      },
+      sass:{
+          dist:{
+              files:{
+                  "css/sass.css":"sass/style.scss"
+              }
+          }
+      },
       watch: {
           css:{
-              files: ['css/style.css'],
-              tasks: ['cssmin']
+              files:["css/style.css", "css/sass.css"],
+              tasks:["csslint", "cssmin"]
+          },
+          js:{
+              files:["js/script.js"],
+              tasks:["jshint", "uglify"]
+          },
+          sass:{
+              files:["sass/style.scss"],
+              tasks:["sass"]
           }
       }
     });
 
     // grunt.loadNpmTasks();
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // grunt.registerTask();
     grunt.registerTask('default', ['cssmin']);
-    grunt.registerTask('min', ['cssmin']);
+    grunt.registerTask('compile', ['sass']);
+    grunt.registerTask('debug', ['jshint', 'csslint']);
+    grunt.registerTask('min', ['csslint', 'cssmin','jshint', 'uglify']);
     grunt.registerTask('w', ['watch']);
 
 
